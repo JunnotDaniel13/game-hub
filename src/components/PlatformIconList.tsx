8eb89a6f25980115fdf8f60434bf1d98f726694b
Junnot Daniel <jerenasonjunnot@gmail.com>
2023-03-27 19:18:26 +0300

    Display platform icons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,43 @@
+import { HStack, Icon } from "@chakra-ui/react";
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+
+import { Platform } from "../hooks/useGame";
+import { IconType } from "react-icons";
+
+interface Props {
+  platforms: Platform[];
+}
+
+function PlatformIconList({ platforms }: Props) {
+  const iconsMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+    android: FaAndroid,
+  };
+
+  return (
+    <HStack marginY={2}>
+      {platforms.map((platform, index) => (
+        <Icon as={iconsMap[platform.slug]} color={"gray.400"} />
+      ))}
+    </HStack>
+  );
+}
+
+export default PlatformIconList;
--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -2,10 +2,17 @@ import { useEffect, useState } from "react";
 import { CanceledError } from "axios";
 import apiClient from "../services/api-client";
 
+export interface Platform {
+  id: string;
+  name: string;
+  slug: string;
+}
+
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[];
 }
 
 interface GamesResponse {
@@ -22,7 +29,9 @@ const useGame = () => {
 
     apiClient
       .get<GamesResponse>("/games", { signal: controller.signal })
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
