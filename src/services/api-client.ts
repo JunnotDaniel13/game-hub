34f55ec1eeb0e8d927e9f3e31c73e53fce7ddedb
Junnot Daniel <jerenasonjunnot@gmail.com>
2023-03-29 10:44:55 +0300

    Add environment variable

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "2f9798b77d5146d493444d67d4652af5",
+    key: process.env.API_KEY,
   },
 });
