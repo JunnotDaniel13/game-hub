66f15c3a289606eb92022c3d6949639a946cb30f
Junnot Daniel <jerenasonjunnot@gmail.com>
2023-03-29 11:57:15 +0300

    Update env variable

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: import.meta.env.VITE_API_KEY,
+    key: "4440825d309f4a52a2be9702cea27ea5",
   },
 });
